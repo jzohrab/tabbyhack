22f389f7d6ad9cd0e572d7ef8e74b54f3f1b81b7
Jeff Zohrab <jzohrab@gmail.com>
2021-03-27 18:05:42 -0700

    Initial commit

---
+++ test/stub-test.js
@@ -0,0 +1,10 @@
+const { join } = require('path')
+const test = require('tape')
+
+// const f = join(process.cwd(), 'app', 'guitarstring.js')
+// const { GuitarString } = require(f)
+
+test('stub', t => {
+  t.equal(5, 5, 'sample')
+  t.end()
+})
