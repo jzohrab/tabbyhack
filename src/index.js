9ddc135762727610c131019be0d2ffb616db02bf
Jeff Zohrab <jzohrab@gmail.com>
2021-03-29 14:51:51 -0700

    Add random note during development.

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ window.listNeckStrings = function() {
 
 /** Start the application */
 window.startRecord = function() {
+  if (window.app) {
+    return
+  }
   window.app = new Application()
   window.app.start()
 }
@@ -21,4 +24,23 @@ window.printRecord = function() {
   const content = JSON.stringify(window.app.notes)
   const display = document.getElementById('recorded')
   display.innerHTML = content
+}
+
+window.onloadBody = function() {
+  // In prod, hide dev tools.
+  // there is probably a much better way to do this,
+  // but this is fine for now.
+  if (process.env.NODE_ENV !== 'development') {
+    document.getElementById('btnAddRandom').style.display = 'none';
+  }
+}
+
+/** Hack helper during dev, add a random frequency. */
+window.addRandom = function() {
+  startRecord()
+  const freq = Math.floor(Math.random() * 200) + 200
+  const n = {
+    name: '?', value: '?', cents: 0, octave: 0, frequency: freq, standard: freq
+  }
+  window.app.update(n)
 }
