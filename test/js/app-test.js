63eaa76a167871b1e7c7746ed90a5de6891b92ec
Jeff Zohrab <jzohrab@gmail.com>
2021-04-15 10:41:16 -0700

    Remove commented tests.

--- test/js/app-test.js
+++ test/js/app-test.js
@@ -76,49 +76,17 @@ test('add_frequency: frets out of range arent included', t => {
   t.end()
 })
 
-// fret candidates out of range
-
 /*
 // Useful frequencies (open strings)
 
 const b = new GuitarString('b', Bhz)
 const e = new GuitarString('e', Ehz)
 
-test('single open string', t => {
-  const rawtab = new Rawtab([b])
-  t.deepEqual(rawtab.tab([Bhz]), [[0]])
-  t.end()
-})
-
 test('single string multiple notes', t => {
   const rawtab = new Rawtab([b])
   const actual = rawtab.tab([Bhz, Ehz, Bhz * 2])
   const expected = [[0], [5], [12]]
   t.deepEqual(actual, expected)
-  t.end()
-})
-
-test('fret less than 0 ignored', t => {
-  const rawtab = new Rawtab([b])
-  t.deepEqual(rawtab.tab([Bhz-100]), [[null]])
-  t.end()
-})
-
-test('fret can be very large if no max fret specified', t => {
-  const rawtab = new Rawtab([b])
-  t.deepEqual(rawtab.tab([Bhz * 16]), [[48]])
-  t.end()
-})
-
-test('fret greater than max ignored', t => {
-  const rawtab = new Rawtab([b], { max: 12 } )
-  t.deepEqual(rawtab.tab([Bhz, Ehz, Ehz * 2, Bhz * 2]), [[0], [5], [null], [12]])
-  t.end()
-})
-
-test('fret less than min ignored', t => {
-  const rawtab = new Rawtab([b], { min: 4, max: 10 } )
-  t.deepEqual(rawtab.tab([Bhz, Ehz, Ehz * 2, Bhz * 2]), [[null], [5], [null], [null]])
   t.end()
 })
 
