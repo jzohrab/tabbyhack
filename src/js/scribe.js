edbfd64d9a777b0f7ffa713e19c5f1fc76a7bda3
Jeff Zohrab <jzohrab@gmail.com>
2021-04-21 18:27:35 -0700

    Updating scribe tests.

--- src/js/scribe.js
+++ src/js/scribe.js
@@ -5,7 +5,7 @@ const Scribe = function(numstrings = 6, scribeOptions = {}) {
 
 
  /**
- * get tab for frequencies
+ * get tab for app line.
  *
  * @param {array} app.line, with .string filled in or not.
  * @returns {array}
@@ -14,6 +14,7 @@ Scribe.prototype.tab = function(appline) {
   const stafflength = this.opts.stafflength || 75
 
   ns = this.numstrings
+
   function newStaff() {
     return new Array(ns).fill('-')
   }
@@ -23,23 +24,37 @@ Scribe.prototype.tab = function(appline) {
   result.push(staff)
 
   for (var i = 0; i < appline.length; i++) {
-    const frets = new Array(ns).fill(null)
 
-    let curr = appline[i]
     let notes = appline[i]
     if (!(notes instanceof Array))
       notes = [ notes ]
 
-    notes.forEach(n => frets[n.string] = n.frets[`${n.string}`])
+    const frets = new Array(ns).fill(null)
+
+    // Fill in frets for all strings.
+    notes.forEach(n => {
+      // Hacky, ensure the string chosen is valid!
+      if (n.string >= 0 && n.string < ns)
+        frets[n.string] = n.frets[`${n.string}`]
+    })
+    console.log(`for line # ${i}, have frets: [ ${frets.join(', ')} ]`)
+
+    let maxfretlen = 0
+    for (var j = 0; j < frets.length; j++) {
+      let fstring = '-'
+      if (frets[j] !== null && frets[j] !== undefined)
+        fstring = `${frets[j]}`
+      frets[j] = fstring
+      if (maxfretlen < fstring.length)
+        maxfretlen = fstring.length
+    }
+    console.log(`after stringing, have frets: [ ${frets.join(', ')} ]`)
     // console.log(frets)
 
-    const maxfret = Math.max(...frets)
-    maxfretlen = `${maxfret}`.length
     const fretstring = frets.
-          map(n => n === null ? '-' : `${n}`).
           map(s => `${'-'.repeat(maxfretlen - s.length)}${s}`).
           map(s => `${s}-`)
-    // console.log(fretstring)
+    console.log(`fretstring = [ ${fretstring.join(', ')} ]`)
     for (var j = 0; j < fretstring.length; j++) {
       staff[j] += fretstring[j]
     }
--- test/js/scribe-test.js
+++ test/js/scribe-test.js
@@ -27,31 +27,45 @@ test('single open string, 2 string ', t => {
   t.end()
 })
 
-/*
-test('single string multiple notes', t => {
-  const scribe = new Scribe(2)
-
-  const b = new Note(Bhz)
-  const e = new Note(Ehz)
-
-
-  const actual = scribe.tab([ { '0': 0 }, {0: 5}, {0: 12} ])
-  const expected = [["-0-5-12-"]]
-  t.deepEqual(actual, expected)
+test('single string many notes', t => {
+  const scribe = new Scribe(1)
+  const line = [
+    makeFrettedNote({0: 0}, 0),
+    makeFrettedNote({0: 5}, 0),
+    makeFrettedNote({0: 12}, 0)
+  ]
+  t.deepEqual(scribe.tab(line), [["-0-5-12-"]])
   t.end()
 })
 
-/*
 
 test('null tabbed as empty', t => {
   const scribe = new Scribe(1)
-  t.deepEqual(scribe.tab([{0: 0}, {0: 5}, {0: null}, {0: 12}]), [["-0-5---12-"]])
+  const line = [
+    makeFrettedNote({0: 0}, 0),
+    makeFrettedNote({0: null}, 0),
+    makeFrettedNote({1: 10}, 0),
+    makeFrettedNote({0: 12}, 0)
+  ]
+  t.deepEqual(scribe.tab(line), [["-0-----12-"]])
   t.end()
 })
 
 test('multistring numbers aligned correctly', t => {
   const scribe = new Scribe(2)
-  const actual = scribe.tab([{1: 0}, {0: 0, 1: 0}, {0: 7, 1: 12}, {1: 5}])
+  const line = [
+    makeFrettedNote({1: 0}, 1),
+    [
+      makeFrettedNote({0: 0}, 0),
+      makeFrettedNote({1: 0}, 1)
+    ],
+    [
+      makeFrettedNote({1: 12}, 1),
+      makeFrettedNote({0: 7}, 0),
+    ],
+    makeFrettedNote({1: 5}, 1)
+  ]
+  const actual = scribe.tab(line)
   const expected = [[
     "---0--7---",
     "-0-0-12-5-"
@@ -62,7 +76,19 @@ test('multistring numbers aligned correctly', t => {
 
 test('multistring null notes arent tabbed', t => {
   const scribe = new Scribe(2)
-  const actual = scribe.tab([{1: 0}, {0: null, 1: 0}, {0: 7, 1: 12}, {1: 5}])
+  const line = [
+    makeFrettedNote({1: 0}, 1),
+    [
+      makeFrettedNote({0: 0}, 4),
+      makeFrettedNote({1: 0}, 1)
+    ],
+    [
+      makeFrettedNote({1: 12}, 1),
+      makeFrettedNote({0: 7}, 0),
+    ],
+    makeFrettedNote({1: 5}, 1)
+  ]
+  const actual = scribe.tab(line)
   const expected = [[
     "------7---",
     "-0-0-12-5-"
@@ -70,6 +96,8 @@ test('multistring null notes arent tabbed', t => {
   t.deepEqual(actual, expected)
   t.end()
 })
+
+/*
 
 test('can split tab to separate lines', t => {
   const scribe = new Scribe(2, { stafflength: 7 })
